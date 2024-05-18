403fb6e84ad303b1d88cc08fbae991103a12a914
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-05-18 18:29:53 -0400

    :bug: [fix] req-02-find_file_by_name

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -17,11 +17,10 @@ def find_file_by_name(context, search_term, case_sensitive=True):
     for path in context["all_files"]:
         file_name = path.split("/")[-1]
 
-        if not case_sensitive:
-            file_name.lower()
-            search_term.lower()
+        if case_sensitive == False:
+            file_name = file_name.lower()
+            search_term = search_term.lower()
 
         if search_term in file_name:
             found_files.append(path)
-
     return found_files
